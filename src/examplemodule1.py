a2781f554489904272e9ef9bef6f7cd49bcb81ea
Shawn Vera <shawnvera@gmail.com>
2023-07-12 18:38:41 +0000

    added first feature and testing

---
+++ src/examplemodule1.py
@@ -0,0 +1,6 @@
+class Character:
+        def __init__(self):
+                self.name = "Cletus"
+        
+        def set_name(self, n):
+            self.name = n
--- src/tests/test_iteration1.py
+++ src/tests/test_iteration1.py
@@ -1,13 +1,15 @@
+from examplemodule1 import Character
+ 
 
-from 
 # test feature create a character
 # can get a name 
 # can set a name 
 
-def test_get_name(name):
+def test_get_name():
     char = Character()
-    assert char.name == name
+    assert char.name == "Cletus"
 
-def test_set_name(name):
-    char.name = name
-    assert char.name == name 
+def test_set_name():
+    char2 = Character()
+    char2.name = "Better Cletus"
+    assert char2.name == "Better Cletus" 
