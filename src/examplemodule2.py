bf9a7c23204886f185d554641f30dbab39761d53
Shawn Vera <shawnvera@gmail.com>
2023-07-13 21:54:07 +0000

    updated test for iteration 2

--- src/examplemodule2.py
+++ src/examplemodule2.py
@@ -2,29 +2,25 @@ from examplemodule1 import Character
 
 class Monk(Character):
     def __init__(self):
-        def hp_on_level():
-            if level == 2:
-                Monk.Character.hp = Monk.Character.hp + 6
+        self.hp = 6
         self.attack_damage = 3
         self.base_att_roll_on_level = +1 
+        self.type = "Chicken"
         
-class Fighter():
-    def __init__(self):
+class Fighter(Character):
+    def __init__(self, level=1, roll=19, armor=10):
         self.hp = 10
-        # self.
-class Rogue():
+        self.attack_power = 6
+        #self.level = 1
+        #self.roll = 19
+
+class Rogue(Character):
   def __init__(self):
       def crit_strike():
           if att_roll == 20:
               attack_damage * 3
 
-
-
-
-# class Classes(Character):
-#     def __init__(self):
-#         self.name = name
-
-#     super().__init__(self, type)
-#     type.monk.hp = Character[hp + 1]
+class Paladin(Character):
+    def __init__(self):
+        self.hp = 9
 
