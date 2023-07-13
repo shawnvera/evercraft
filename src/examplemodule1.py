2ba7d5ba0acb41869a72e56da48fdb90a9a17313
Shawn Vera <shawnvera@gmail.com>
2023-07-13 14:50:57 +0000

    updated pytests

--- src/examplemodule1.py
+++ src/examplemodule1.py
@@ -7,32 +7,15 @@ class Character:
                 self.attack_power = 5
                 self.roll = 19
                 self.attributes = {
-                    "strength": 10,#mod(),
+                    "strength": 10,
                     "dexterity": 10,
                     "constitution": 10,
                     "wisdom": 10,
                     "intelligence": 10,
                     "charisma": 10
                 }
-        
-        # def get_name(self, n):
-        #     self.name = "Cletus"
+                self.xp = 0
+                self.level = 1
 
 
-        # def set_name(self, n):
-        #     self.name = n
-
-        # def set_alignment(self, alignment):
-        #     self.alignment = "good", "evil", "neutral"
-        
-        # def armor_class(self, armor_class):
-        #     self.armor_class = 10
-
-        # def hp(self, hp):
-        #     self.hp = 5
-
-        def mod():
-            strength = 10
-            for i in range(20):
-                yield strength + 4
-
+                
